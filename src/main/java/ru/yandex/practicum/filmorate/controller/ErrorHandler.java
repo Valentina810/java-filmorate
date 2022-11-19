49456c2f343b468923c3a006a4c26990aae6aca3
valentinakolesnikova <polikarpova-v@yandex.ru>
2022-11-19 22:56:58 +0300

    feat: добавить логирование в класс ErrorHandler

--- src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/ErrorHandler.java
@@ -1,5 +1,6 @@
 package ru.yandex.practicum.filmorate.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -8,24 +9,28 @@ import ru.yandex.practicum.filmorate.exception.EntityNotFoundException;
 import ru.yandex.practicum.filmorate.exception.EntityValidationException;
 import ru.yandex.practicum.filmorate.model.ErrorResponse;
 
+@Slf4j
 @RestControllerAdvice
 public class ErrorHandler {
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorResponse handleIncorrectParameterException(final EntityNotFoundException e) {
+		log.error(e.getMessage());
 		return new ErrorResponse(e.getMessage());
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
 	public ErrorResponse handleInvalidEmailException(final EntityValidationException e) {
+		log.error(e.getMessage());
 		return new ErrorResponse(e.getMessage());
 	}
 
 	@ExceptionHandler
 	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
 	public ErrorResponse handleThrowable(final RuntimeException e) {
+		log.error(e.getMessage());
 		return new ErrorResponse("Произошла непредвиденная ошибка: " + e.getMessage());
 	}
 }
